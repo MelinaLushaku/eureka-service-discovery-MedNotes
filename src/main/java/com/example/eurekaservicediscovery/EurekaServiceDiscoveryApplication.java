8b87f807bd162c124cb9199b036733f39aa61929
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-10-20 21:00:39 +0200

    Service Discovery and Register - 5.1

--- src/main/java/com/example/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
+++ src/main/java/com/example/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceDiscoveryApplication {
-
+//shto git
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
     }
